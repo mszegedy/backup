fd0bfb1a5c305e68f9caf6942987bb2ef35fbf80
Maria Szegedy <mszegedy2@gmail.com>
2019-06-17 18:05:44 -0400

    Added ~/oldschool to backed up directories

--- backup.sh
+++ backup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 rsync -avz --delete ~/code                ~/Dropbox/backup
-rsync -avz --delete ~/Documents/lists     ~/Dropbox/backup
 rsync -avz --delete ~/Documents/pdfs      ~/Dropbox/backup
 rsync -avz --delete ~/Documents/important ~/Dropbox/backup
 rsync -avz --delete ~/school              ~/Dropbox/backup
-rsync -avz --delete ~/research            ~/Dropbox/backup
+rsync -avz --delete ~/oldschool           ~/Dropbox/backup
